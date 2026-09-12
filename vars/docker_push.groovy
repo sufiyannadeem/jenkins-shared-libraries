f4051c85943cc515f8e2c5ff7686afdfe5eba5e6
Mohammed Nadeem Sufiyan <sufiyanmohammed098@gmail.com>
2026-09-12 19:13:24 +0530

    Refactor Docker image push logic
    Refactor Docker push function to use withEnv for environment variables and correct credentials name.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,19 +1,36 @@
 def call(Map config = [:]) {
+
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'docker-hub-credentials'
-    
+    def credentials = config.credentials ?: 'dockerhub-credentails'
+
     echo "Pushing Docker image: ${imageName}:${imageTag}"
-    
-    withCredentials([usernamePassword(
-        credentialsId: credentials,
-        usernameVariable: 'DOCKER_USERNAME',
-        passwordVariable: 'DOCKER_PASSWORD'
-    )]) {
-        sh """
-            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
-            docker push ${imageName}:${imageTag}
-            docker push ${imageName}:latest
-        """
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: credentials,
+            usernameVariable: 'DOCKER_USERNAME',
+            passwordVariable: 'DOCKER_PASSWORD'
+        )
+    ]) {
+
+        withEnv([
+            "IMAGE_NAME=${imageName}",
+            "IMAGE_TAG=${imageTag}"
+        ]) {
+
+            sh '''
+                set -e
+
+                echo "$DOCKER_PASSWORD" | docker login \
+                    -u "$DOCKER_USERNAME" \
+                    --password-stdin
+
+                docker push "$IMAGE_NAME:$IMAGE_TAG"
+                docker push "$IMAGE_NAME:latest"
+            '''
+        }
     }
 }
+
+
